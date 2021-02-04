1117d3f69be7fbe20ee7da8a300f9bf503eb6ebf
RossieK <rosi.kirova96@gmail.com>
2021-02-04 23:06:49 +0200

    Add user controller and login page display

--- routes.js
+++ routes.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
 const homeController = require('./controllers/homeController');
 const shoesController = require('./controllers/shoesController');
+const userController = require('./controllers/userController');
 
 const router = Router();
 
 router.use('/', homeController);
 router.use('/shoes', shoesController);
+router.use('/user', userController);
 
 module.exports = router;
