f7c6ca0f8e849d28521b4bac8832e750fbff6bc9
RossieK <rosi.kirova96@gmail.com>
2021-02-04 23:07:58 +0200

    Add register view display

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,4 +6,8 @@ router.get('/login', (req, res) => {
     res.render('login', { title: 'Login Page' });
 });
 
+router.get('/register', (req, res) => {
+    res.render('register', { title: 'Register Page' });
+});
+
 module.exports = router;
